a88b90be2161ab06051efe0baa00b2152d42aa8a
Trenton Scott <InsaneGriot@Outlook.com>
2025-03-17 16:38:10 -0400

    Adding navigation

---
+++ GenericNewGame/Navigation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Stride.Input;
+using Stride.Engine;
+using Stride.Navigation;
+
+namespace GenericNewGame
+{
+    public class Navigation : SyncScript
+    {
+        public Entity character;
+        public Entity sphereToClone;
+
+        private NavigationComponent navigation;
+        private List<Entity> waypointSpheres;
+
+        public override void Start()
+        {
+            navigation = character.Get<NavigationComponent>();
+        }
+
+        public override void Update()
+        {
+            if (Input.IsMouseButtonPressed(MouseButton.Left))
+            {
+                SetNewTarget();
+            }
+        }
+
+        private void SetNewTarget()
+        {
+
+        }
+    }
+}
